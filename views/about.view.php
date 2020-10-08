d6c52b66cbc661e760c97ce3a47d2c7defe2bb86
LaupWing <loc_strict@hotmail.com>
2020-10-08 17:45:59 +0200

    feractore views

--- views/about.view.php
+++ views/about.view.php
@@ -1,6 +1,3 @@
 <?php require 'partials/head.php' ?>
-	<?php require 'partials/nav.php' ?>
-	<h1>About</h1>
-</body>
-
-</html>
+<h1>About</h1>
+<?php require 'partials/footer.php' ?>
--- views/index.view.php
+++ views/index.view.php
@@ -1,15 +1,12 @@
 <?php require 'partials/head.php' ?>
-	<?php require 'partials/nav.php' ?>
-	<h1>My Tasks</h1>
-   <?php foreach ($tasks as $task) : ?>
-      <li>
-         <?php if ($task->completed) : ?>
-            <strike><?= $task->description; ?></strike>
-         <?php else : ?>
-            <?= $task->description; ?>
-         <?php endif; ?>
-      </li>
-   <?php endforeach; ?>
-</body>
-
-</html>
+<h1>My Tasks</h1>
+<?php foreach ($tasks as $task) : ?>
+   <li>
+      <?php if ($task->completed) : ?>
+         <strike><?= $task->description; ?></strike>
+      <?php else : ?>
+         <?= $task->description; ?>
+      <?php endif; ?>
+   </li>
+<?php endforeach; ?>
+<?php require 'partials/footer.php' ?>
