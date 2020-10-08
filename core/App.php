7c6a07a628d7edaf5c2b0a64f8527be514ee8303
LaupWing <loc_strict@hotmail.com>
2020-10-08 21:42:43 +0200

    main app injection

---
+++ core/App.php
@@ -0,0 +1,18 @@
+<?php
+
+
+class App
+{
+   protected static $registery = [];
+   public static function bind($key, $value)
+   {
+      static::$registery[$key] = $value;
+   }
+   public static function get($key)
+   {
+      if (!array_key_exists($key, static::$registery)) {
+         throw new Exception("Key {$key} is not boudn to this class");
+      }
+      return static::$registery[$key];
+   }
+}
