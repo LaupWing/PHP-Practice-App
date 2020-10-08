44854c65e35c11504df2f613b130c1f99f7d4a7a
LaupWing <loc_strict@hotmail.com>
2020-10-08 21:07:08 +0200

    update

--- views/index.view.php
+++ views/index.view.php
@@ -1,7 +1,9 @@
 <?php require 'partials/head.php' ?>
 
 <h1>Submit Your Name</h1>
-
+<?php foreach ($users as $user) : ?>
+   <li><?= $user->name ?></li>
+<?php endforeach ?>
 <form method="POST" action="/names">
    <input type="text" name="name">
 </form>
