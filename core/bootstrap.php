f0ae0a56e19946dc07433b5ef85b6d4a48e9ca4c
LaupWing <loc_strict@hotmail.com>
2020-10-09 09:06:51 +0200

    works

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,18 +1,18 @@
 <?php
 
-$App::bind('config', require 'config.php');
+App::bind('config', require 'config.php');
 
 // require 'core/Router.php';
 // require 'core/Request.php';
 // require 'core/database/Connection.php';
 // require 'core/database/QueryBuilder.php';
 
-$App::bind('database',  new QueryBuilder(
-   Connection::make($App::get('config')['database'])
+App::bind('database',  new QueryBuilder(
+   Connection::make(App::get('config')['database'])
 ));
 
 function view($name, $data = [])
 {
    extract($data);
-   return require "views/{$name}.view.php";
+   return "views/{$name}.view.php";
 }
